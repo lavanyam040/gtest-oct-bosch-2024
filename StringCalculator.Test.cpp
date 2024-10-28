1d06414829f410c81a9b733a199087d2bc35c1d4
lavanyam040 <69637515+lavanyam040@users.noreply.github.com>
2024-10-28 15:22:25 +0530

    Create StringCalculator.Test.cpp

---
+++ StringCalculator.Test.cpp
@@ -0,0 +1,12 @@
+#include <StringCalculator.h>
+#include <gtest/gtest.h>
+
+TEST(string_calculator_add,when_passed_a_single_number_return_0_for_Empty){
+
+StringCalculator objUnderTest;
+string input="";
+int expectedValue=0;
+
+int actualValue =objUnderTest Add(input);
+ASSERT_EQ(expectedValue,actualValue);
+}
