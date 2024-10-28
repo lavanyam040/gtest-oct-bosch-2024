a0d1bcf8370a0b7a71dbc596e51be32048c73224
lavanyam040 <69637515+lavanyam040@users.noreply.github.com>
2024-10-28 15:30:53 +0530

    Update StringCalculator.Test.cpp

--- StringCalculator.Test.cpp
+++ StringCalculator.Test.cpp
@@ -10,3 +10,12 @@ int expectedValue=0;
 int actualValue =objUnderTest Add(input);
 ASSERT_EQ(expectedValue,actualValue);
 }
+TEST(string_calculator_add,when_passed_a_single_number_return_0_for_ZERO){
+
+StringCalculator objUnderTest;
+string input="";
+int expectedValue=0;
+
+int actualValue =objUnderTest Add(input);
+ASSERT_EQ(expectedValue,actualValue);
+}
