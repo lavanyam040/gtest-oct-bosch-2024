b1725b98287006ad02861c1675b1d9d295c9d9ab
lavanyam040 <69637515+lavanyam040@users.noreply.github.com>
2024-10-29 11:49:38 +0530

    Update TestChallenge.Test.cpp

--- TestChallenge.Test.cpp
+++ TestChallenge.Test.cpp
@@ -1,1 +1,18 @@
+#include <StringCalculator.h>
+#include <gtest/gtest.h>
 
+Class AdditionTest:public testting ::Test{
+  protected :
+  AdditionTest *objUnderTest
+  int result = 0;
+  void SetUp() override {
+  objUnderTest = new AdditionTest();
+}
+  void TearDown() {
+  delete  objUnderTest;
+  }
+};
+TEST(AdditionTest, AddsTwoPositiveNumbers) {
+    add(2,2 , &result);
+    EXPECT_EQ(result, 4);
+}
